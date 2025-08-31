47a62d5ca2ee4a77571b6439f454caa95c45c0f8
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-08-31 16:45:32 -0400

    Override player prefab

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
+public class PlayerCombat: MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
