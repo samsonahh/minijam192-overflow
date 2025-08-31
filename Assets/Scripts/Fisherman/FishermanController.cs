76c078f3ddf36c80f17bc3bad9e6d15e8a350213
Samson Wu <samsonwu05@gmail.com>
2025-08-30 20:35:03 -0400

    Create fisherman

---
+++ Assets/Scripts/Fisherman/FishermanController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Fisherman
+{
+    public class FishermanController : MonoBehaviour
+    {
+        [field: SerializeField] public Animator Animator { get; private set; }
+
+        public StateMachine<FishermanController> StateMachine { get; private set; }
+
+        [field: Header("States")]
+        [field: SerializeField] public IdleState IdleState { get; private set; } = new();
+        [field: SerializeField] public HarpoonState HarpoonState { get; private set; } = new();
+
+        private void Awake()
+        {
+            SetupStateMachine();
+        }
+
+        private void SetupStateMachine()
+        {
+            StateMachine = new StateMachine<FishermanController>(this);
+
+            IdleState.Init(this, StateMachine);
+            HarpoonState.Init(this, StateMachine);
+
+            StateMachine.ChangeState(IdleState);
+        }
+
+        private void OnDestroy()
+        {
+            StateMachine.Destroy();
+        }
+
+        private void Update()
+        {
+            StateMachine.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            StateMachine.FixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/Fisherman/HarpoonState.cs
@@ -0,0 +1,26 @@
+namespace Fisherman
+{
+    [System.Serializable]
+    public class HarpoonState : State<FishermanController>
+    {
+        public override void Enter()
+        {
+
+        }
+
+        public override void Exit()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void FixedUpdate()
+        {
+
+        }
+    }
+}
